2af5029204a83512513f629e4686d133607e6c2c
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-03-25 02:34:31 -0500

    Add placeholder components for header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//TODO: Add content
+
+export default function Footer() {
+  return (
+    <footer>
+
+    </footer>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//TODO: Add content
+
+export default function Headers() {
+  return (
+    <header className="App-header">
+    </header>
+  );
+}
