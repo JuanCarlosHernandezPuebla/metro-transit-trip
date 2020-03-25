becb2b5aed9746b5e65f5edf5a804e0e9be9f502
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-03-25 02:37:19 -0500

    Add initial Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+const bundlePath = path.resolve(__dirname, "dist/");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ["@babel/plugin-proposal-object-rest-spread"]
+        }
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    publicPath: bundlePath,
+    filename: "bundle.js"
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'public'),
+    port: 8080,
+    open: true,
+    publicPath: "http://localhost:8080/dist"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
