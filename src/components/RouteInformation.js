17ad7c7f4716797bb6d07a1dc0cfb0e3df181d42
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-03-26 00:13:54 -0500

    Set direction enum values

--- src/components/RouteInformation.js
+++ src/components/RouteInformation.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Form from "react-jsonschema-form";
-import * as RouteInformationForm from '../forms/RouteInformation.json'
+import * as RouteInformationForm from '../forms/RouteInformation.json';
 import { config } from '../config';
 import { cloneDeep, set } from 'lodash';
 
@@ -13,6 +13,8 @@ const metroTransitEntry = config['metroTransitEntry'];
 
 const routesEndPoint = `${metroTransitEntry}${config['endpoints']['routes']}`;
 
+const directionsEndPoint = `${metroTransitEntry}${config['endpoints']['directions']}`;
+
 export default function RouteInformation() {
 
   const [formData, setFormData] = useState({});
@@ -24,6 +26,8 @@ export default function RouteInformation() {
 
   const [routes, setRoutes] = useState([]);
 
+  const [directions, setDirections] = useState([]);
+
   useEffect(() => loadRoutes(), []);
 
   useEffect(() => {
@@ -31,6 +35,12 @@ export default function RouteInformation() {
     setRouteEnums(newForm, 'schema.properties.route');
     setForm(newForm);
   }, [routes]);
+
+  useEffect(() => {
+    const newForm = _.cloneDeep(form);
+    setDirectionEnums(newForm, 'schema.properties.direction');
+    setForm(newForm);
+  }, [directions]);
 
   const loadRoutes = () => {
     fetch(routesEndPoint, {
@@ -41,12 +51,32 @@ export default function RouteInformation() {
     .catch(error => console.error('Error: ', error))
   };
 
+  const loadDirections = (route) => {
+    fetch(directionsEndPoint.replace('{route}', route), {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => setDirections(data))
+    .catch(error => console.error('Error: ', error))
+  }
+
   const setRouteEnums = (form, path) => {
     _.set(form, path + '.enum', routes.map(route => route.RouteId));
     _.set(form, path + '.enumNames', routes.map(route => route.Description));
   };
 
-  //TODO: Add remaining inputs and apply styling. 
+  const setDirectionEnums = (form, path) => {
+    _.set(form, path + '.enum', directions.map(route => route.DirectionId));
+    _.set(form, path + '.enumNames', directions.map(route => route.DirectionName));
+  };
+
+  const onChange = formData => {
+    const { route } = formData;
+    loadDirections(route);
+    setFormData(formData);
+  };
+
+  //TODO: Add remaining inputs and apply styling.
 
   return (
     <div className="container">
@@ -57,7 +87,7 @@ export default function RouteInformation() {
             schema={form.schema}
             uiSchema={form.uiSchema}
             formData={formData}
-            onChange={({ formData }) => setFormData(formData)}
+            onChange={({formData}) => onChange(formData)}
             onSubmit={() => { }}
             onError={() => { }}
           />
