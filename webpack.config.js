6c8334b67b59fe8663e3e9d14b7f23754ac1911f
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-03-29 01:35:32 -0500

    Add Sass to webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
           plugins: ["@babel/plugin-proposal-object-rest-spread"]
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
