5e5d47049e75dea5a30658ca5d47246c11b02e70
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2020-04-04 22:23:25 -0500

    Import ThemeProvider for future use

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { ThemeProvider } from '@material-ui/core';
+
+// TODO: Add Custom Theme
+
+const theme = {};
 
 export default function App() {
   return (
-    <div className='App'>
+    <div>
       <Header />
       <Main />
       <Footer />
